a5b0ddccc68cd5fc8c0dd3ddb9cfb98a4bc0b6bd
Matthew Sherman <msherman0801@gmail.com>
2019-02-14 09:04:42 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+    def self.current_user(param)
+        User.find(param[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        true if session.include?(:user_id)
+    end
+
 end
