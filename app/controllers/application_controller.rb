cf1946c52bd38178197cdee2c0beefa9773c238e
Johnathon Wood <jaywood128@gmail.com>
2019-02-16 19:37:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,17 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params["username"])
     session[:user_id] = @user.id
+<<<<<<< HEAD
 
     if !!@user == true
        redirect to '/account'
     else
       erb :error
     end
+=======
+>>>>>>> 2362f3f9491633f3bb7e698d30826888c212f525
 
+    redirect to '/account'
   end
 
   get '/account' do
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,7 +6,11 @@ class Helpers
   end
 
   def self.is_logged_in?(hash)
+<<<<<<< HEAD
 
     !!current_user(hash)
+=======
+    !!current_user
+>>>>>>> 2362f3f9491633f3bb7e698d30826888c212f525
   end
 end
